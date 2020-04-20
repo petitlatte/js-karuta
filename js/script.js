d6b14a777bbfe24761e60aa1695cd254db2f927e
petitlatte <lisasumida@gmail.com>
2020-04-20 21:38:57 +1200

    added placeholder cards

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+//FADE IN OUT CONTAINERS
+$(document).ready(function(){
 
+$('#button-start').click(function(e){    
+    $('.conA').fadeOut('slow', function(){
+        $('.conB').fadeIn('slow');
+    });
+});
+
+
+});
